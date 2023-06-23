9e55be112d0bbbf1fee5ddaa975e1875fa4340f0
Rodolfo Fialho da Costa <rodolfofialho905@gmail.com>
2023-06-22 22:16:11 -0300

    add scripts/ operador ternario e obj Date

---
+++ lógica com js/aula03/operador_ternario.js
@@ -0,0 +1,13 @@
+// // ? :
+
+pontuacao = 900;
+
+const nivel = pontuacao >= 1000 ? 'Usuario VIP' : 'Usuario normal';
+
+console.log(nivel)
+
+// if (pontuacao >= 1000) {
+//     console.log('Usuario vip');
+// } else {
+//     console.log('usuario normal')
+// }
---
+++ lógica com js/aula04/objeto_Date.js
@@ -0,0 +1,9 @@
+
+// const tresHora = 60 * 60 * 3 * 1000;
+// const umDia = 60 * 60 * 24 * 1000;
+// const data = new Date(0 + tresHora + umDia);
+// console.log(data.toString());
+
+const data = new Date();
+// Data atual do sistema operacional
+console.log(data.toString());
