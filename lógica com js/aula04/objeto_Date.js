604130e40f1c1e975e033b185262a8d6a76a4a1f
Rodolfo Fialho da Costa <rodolfofialho905@gmail.com>
2023-06-26 19:52:05 -0300

    add aula de switch_case

--- lógica com js/aula04/objeto_Date.js
+++ lógica com js/aula04/objeto_Date.js
@@ -1,9 +1,38 @@
 
-// const tresHora = 60 * 60 * 3 * 1000;
-// const umDia = 60 * 60 * 24 * 1000;
-// const data = new Date(0 + tresHora + umDia);
+// // const tresHora = 60 * 60 * 3 * 1000;
+// // const umDia = 60 * 60 * 24 * 1000;
+// // const data = new Date(0 + tresHora + umDia);
+// // console.log(data.toString());
+
+// const data = new Date('2019-04-20 20:20:59');
+// // Data atual do sistema operacional
+
+// console.log('Dia', data.getDate());
+// console.log('Mes', data.getMonth());
+// console.log('ano', data.getFullYear());
+// console.log('Min', data.getMinutes());
+// console.log('Seg', data.getSeconds());
+// console.log('ms', data.getMilliseconds());
+// console.log('Dia Semana', data.getDay());
 // console.log(data.toString());
 
+
+function addZero(num) {
+    return num >= 10 ? num : `0${num}`
+}
+
+
+function formataData(data) {
+    const dia = addZero(data.getDate());
+    const mes = addZero(data.getMonth() + 1);
+    const ano = addZero(data.getFullYear());
+    const hora = addZero(data.getHours());
+    const min = addZero(data.getMinutes());
+    const seg = addZero(data.getSeconds());
+
+    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`
+}
+
 const data = new Date();
-// Data atual do sistema operacional
-console.log(data.toString());
+const dataBR = formataData(data);
+console.log(dataBR)
---
+++ lógica com js/aula05/switch_case.js
@@ -0,0 +1,5 @@
+const data = new Date();
+
+const diaSemana = data.getDay();
+// 0-6, zero é domingo e seis sábado
+console.log(diaSemana);
